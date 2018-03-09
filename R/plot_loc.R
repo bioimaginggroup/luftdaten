4d7af38f781d1a8214b8ab77222ae09a18ad9b2a
Volker Schmid <volker.schmid@lmu.de>
2018-03-09 09:15:40 +0100

    .

--- R/plot_loc.R
+++ R/plot_loc.R
@@ -1,28 +1,20 @@
 #' Plot locations from database
 #'
 #' @param loc dataframe with lat and lon (latitude and longitude)
-#' @param range vector of length 4, corners of area to plot 
+#' @param bbox bounding box
+#' @param col colour for each point (forwarded to points)
 #'
 #' @export
 #'
-plot_loc<-function(loc, range=c(5.86,15.05,47.27,55.06), col="black", ...)
+plot_loc<-function(loc, bbox=c(range(locid$lat),range(locid$lon))[c(1,3,2,4)], col="black")
 {
   data("germanyborder", package="luftdaten")
-  if (any(is.na(range)))
-    {
-    min.x<-min(c(min.x,loc$lon))
-    max.x<-max(c(max.x,loc$lon))
-    max.y<-max(c(max.y,loc$lat))
-    min.y<-min(c(min.y,loc$lat))
-  }
-  else
-  {
-    min.x<-range[1]
-    max.x<-range[2]
-    min.y<-range[3]
-    max.y<-range[4]
-  }
-  
+
+  min.x<-bbox[2]
+  max.x<-bbox[4]
+  min.y<-bbox[1]
+  max.y<-bbox[3]
+
   xx<-max.x-min.x
   yy<-1.5*(max.y-min.y)
   par(mai=rep(0,4),pin=c(3,3*yy/xx))
@@ -30,6 +22,6 @@ plot_loc<-function(loc, range=c(5.86,15.05,47.27,55.06), col="black", ...)
   for (i in 1:length(germany16))polygon(germany16[[i]],border=grey(0.9),col="transparent")
   for (i in 1:length(germany))polygon(germany[[i]],border=grey(.5))
   par(...)
-  points(loc$lon,loc$lat, pch=19, cex=0.2, col=col)
+  points(loc$lon,loc$lat, pch=19, col=col)
 
 }
